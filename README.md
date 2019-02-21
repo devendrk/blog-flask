1305e14fc05c7b5f56113e1fcdff71830d5ff19f
devendrk <katwaldevendra@hotmail.com>
2019-02-21 13:51:14 +0200

    first-commit

---
+++ README.md
@@ -0,0 +1,9 @@
+https://realpython.com/pipenv-guide/#pipenv-introduction
+
+```Python
+ pip install pipenv
+# cd to the project folder
+ pipenv flask
+ pipenv shell # starts the virtual env
+
+```
---
+++ run.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+posts = [
+  {
+    'author':'Don',
+    'title': "Test post 1",
+    'content': "Test content 1"
+  },
+  {
+    'author':'Don',
+    'title': "Test post 2",
+    'content': "Test content 2"
+  },
+  {
+    'author':'Don',
+    'title': "Test post 3",
+    'content': "Test content 3"
+  },
+]
+app.route('/')
+def home():
+    return render_template('home.html', posts = posts)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
