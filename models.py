41d22d790bca9823eb2d902e660a1687994e56ba
danielpatricksexton <danielpatricksexton@gmail.com>
2019-03-04 10:41:24 +0200

    add to model

--- models.py
+++ models.py
@@ -1,8 +1,19 @@
 from service-jobboard import db
 from datetime import datetime
+from flask_sqlalchemy import SQLAalchemy
 
 
 class Jobs(db.Model):
+    id = db.Column(db.Integer)
     company = db.Column(db.String(50), primary_key=True)
     description = db.Column(db.String(200), nullable=False)
     expire = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'company': self.company,
+            'description': self.description,
+            'expire': self.expire
+        }
