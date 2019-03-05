2b56e28b73fbb0a7640416191ef272be5749376d
danielpatricksexton <danielpatricksexton@gmail.com>
2019-03-05 11:17:33 +0200

    change to contact route

--- run.py
+++ run.py
@@ -23,7 +23,7 @@ posts = [
 def home():
   return render_template('home.html', posts = posts)
 
-@contact.route('/contact')
+@app.route('/contact')
 def contact():
   return render_template('contact.html')
 
